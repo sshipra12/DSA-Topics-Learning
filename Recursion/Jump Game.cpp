1307ce2dab5598acab977862bf0bdb3e27e6dd22
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-10-09 11:00:02 +0530

    Update Jump Game.cpp

--- Recursion/Jump Game.cpp
+++ Recursion/Jump Game.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
+    // state definition: can we reach from current ith index to target index (i.e n-1 index)
+    bool canReachFromIthToLast(int index, vector<int> &nums){
+        //base case: where transition fails
+        if(index==nums.size()-1)
+            return true;
+        
+        // transition: of all choices from here, check if from anyone of them can we reach target
+        int jumpLength;
+        for(jumpLength=1;jumpLength<=nums[index]&&index+jumpLength<=nums.size()-1;jumpLength++){
+            if(canReachFromIthToLast(index+jumpLength,nums))
+                return true;
+        }
+        return false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        return canReachFromIthToLast(0,nums);
+    }
+};
+
+
+// Old solution
+/*
+class Solution {
+public:
     bool canReachFromIthToTarget(int index, vector<int> &nums){
+        
+        // TDOD: solution in better time complexity
         // base case: when we have reached target or beyond it
         if(index==nums.size()-1)
             return true;
@@ -18,6 +45,8 @@ public:
 
     bool canJump(vector<int>& nums) {
         // understand state/subproblem meaning and provide required inputs to get answer
+        
         return canReachFromIthToTarget(0,nums);
     }
 };
+*/
